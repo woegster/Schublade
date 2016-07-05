4bf3a8f068156dddd11ba26013fb45a8c61e7dd6
woegster <tobias.woegerbauer@outlook.com>
2016-07-05 17:54:18 +0200

    fixed Int Dir added empty unit test

---
+++ UnitTest/unittest1.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SchubladenUnitTest
+{
+  TEST_CLASS(UnitTest1)
+  {
+  public:
+  };
+}
